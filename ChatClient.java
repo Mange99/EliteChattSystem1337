26f721c931ba23f99f429f882f4a94b6c1aae1e7
vlbins <albinlandgreen@gmail.com>
2019-01-11 09:16:27 +0100

    hello

--- ChatClient.java
+++ ChatClient.java
@@ -1,32 +1,37 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 /**
- * A simple Swing-based client for the chat server.  Graphically
- * it is a frame with a text field for entering messages and a
- * textarea to see the whole dialog.
+ * A simple Swing-based client for the chat server. Graphically it is a frame
+ * with a text field for entering messages and a textarea to see the whole
+ * dialog.
  *
- * The client follows the Chat Protocol which is as follows.
- * When the server sends "SUBMITNAME" the client replies with the
- * desired screen name.  The server will keep sending "SUBMITNAME"
- * requests as long as the client submits screen names that are
- * already in use.  When the server sends a line beginning
- * with "NAMEACCEPTED" the client is now allowed to start
- * sending the server arbitrary strings to be broadcast to all
- * chatters connected to the server.  When the server sends a
- * line beginning with "MESSAGE " then all characters following
- * this string should be displayed in its message area.
+ * The client follows the Chat Protocol which is as follows. When the server
+ * sends "SUBMITNAME" the client replies with the desired screen name. The
+ * server will keep sending "SUBMITNAME" requests as long as the client submits
+ * screen names that are already in use. When the server sends a line beginning
+ * with "NAMEACCEPTED" the client is now allowed to start sending the server
+ * arbitrary strings to be broadcast to all chatters connected to the server.
+ * When the server sends a line beginning with "MESSAGE " then all characters
+ * following this string should be displayed in its message area.
  */
 public class ChatClient {
 
@@ -35,34 +40,58 @@ public class ChatClient {
     JFrame frame = new JFrame("Chatter");
     JTextField textField = new JTextField(40);
     JTextArea messageArea = new JTextArea(8, 40);
+    int focusKeeper = 0;
+    //Färger för GUI
+    public static final Color FIELD_C = new Color(89, 89, 89);
+    public static final Color MESSAGE_C = new Color(58,58,58);
 
     /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Return in the
-     * listener sends the textfield contents to the server.  Note
-     * however that the textfield is initially NOT editable, and
-     * only becomes editable AFTER the client receives the NAMEACCEPTED
-     * message from the server.
+     * Constructs the client by laying out the GUI and registering a listener with
+     * the textfield so that pressing Return in the listener sends the textfield
+     * contents to the server. Note however that the textfield is initially NOT
+     * editable, and only becomes editable AFTER the client receives the
+     * NAMEACCEPTED message from the server.
      */
     public ChatClient() {
 
         // Layout GUI
         textField.setEditable(false);
+        textField.setBorder(null);
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        messageArea.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        textField.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        textField.setBackground(FIELD_C);
+        textField.setForeground(Color.white);
+        Font font = new Font("Lucida Sans Typewriter", Font.BOLD, 14);
+        messageArea.setFont(font);
+        textField.setFont(font);
         messageArea.setEditable(false);
-        frame.getContentPane().add(textField, "North");
+        messageArea.setForeground(Color.white);
+        messageArea.setBackground(MESSAGE_C);
+        frame.getContentPane().add(textField, "South");
         frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        
         frame.pack();
-
+        frame.setSize(800, 600);
         // Add Listeners
         textField.addActionListener(new ActionListener() {
             /**
-             * Responds to pressing the enter key in the textfield by sending
-             * the contents of the text field to the server.    Then clear
-             * the text area in preparation for the next message.
+             * Responds to pressing the enter key in the textfield by sending the contents
+             * of the text field to the server. Then clear the text area in preparation for
+             * the next message.
              */
             public void actionPerformed(ActionEvent e) {
-                out.println(textField.getText());
+                if(textField.getText().length() < 10) {
+                    out.println(textField.getText());
+                }
+                else if(textField.getText().substring(0,10).contains("!whisper")) {
+                    out.println(textField.getText().substring(9));    
+                }
+                else {
+                    out.println(textField.getText());
+                }
                 textField.setText("");
+
             }
         });
     }
@@ -71,22 +100,22 @@ public class ChatClient {
      * Prompt for and return the address of the server.
      */
     private String getServerAddress() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Enter IP Address of the Server:",
-            "Welcome to the Chatter",
-            JOptionPane.QUESTION_MESSAGE);
+        
+        /*JLabel inputText = new JLabel();*/
+        /*return JOptionPane.showInputDialog(frame, "Enter IP", "Welcome to the Chatter",
+                JOptionPane.QUESTION_MESSAGE);*/
+                return "127.0.0.1";
     }
 
     /**
      * Prompt for and return the desired screen name.
      */
     private String getName() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Choose a screen name:",
-            "Screen name selection",
-            JOptionPane.PLAIN_MESSAGE);
+
+/*        return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
+                JOptionPane.PLAIN_MESSAGE);*/
+
+                return "albin";
     }
 
     /**
@@ -97,8 +126,7 @@ public class ChatClient {
         // Make connection and initialize streams
         String serverAddress = getServerAddress();
         Socket socket = new Socket(serverAddress, 9001);
-        in = new BufferedReader(new InputStreamReader(
-            socket.getInputStream()));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
 
         // Process all messages from server, according to the protocol.
@@ -108,6 +136,7 @@ public class ChatClient {
                 out.println(getName());
             } else if (line.startsWith("NAMEACCEPTED")) {
                 textField.setEditable(true);
+                focusKeeper = 1;
             } else if (line.startsWith("MESSAGE")) {
                 messageArea.append(line.substring(8) + "\n");
             }
